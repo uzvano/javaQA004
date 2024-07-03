30836d81d8e7bd6909efae41da2fcd439d2d4d8c
Ivan <uzvano@mail.ru>
2024-07-03 21:10:21 +0300

    changed Main.java and BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,5 @@
 public class BmiService {
-    public double calculate(double a, double b) {
-        double index = a / (b * b);
-        return (int) index;
+    public double calculate(double weight, double height) {
+        return (int) (weight / (height * height));  // метод возвращает целое число
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double m = 72.5; // масса в килограммах
-        double h = 1.81; // рост в метрах
-        double bmi = service.calculate(m, h);
-        System.out.println(bmi);
+        double weight = 72.5; // масса в килограммах
+        double height = 1.81; // рост в метрах
+        int index = (int) service.calculate(weight, height);
+        System.out.println(index);
     }
 }
